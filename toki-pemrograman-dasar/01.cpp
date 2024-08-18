70dcd680e11bd17c1e958192b7075682ba50fc3a
Ambadaya Gaman <ambadayaakun@gmail.com>
2024-08-18 19:44:19 +0800

    finished 01

---
+++ toki-pemrograman-dasar/01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Halo TLX" << std::endl << "Ini dibawah ygy" << std::endl;
+  std::cout << "Pak Dengklek memiliki " << 75 << " ekor sapi." << std::endl;
+  std::cout << "18 dikali 25 dibagi 10 sama dengan " << 18 * 25 / 10
+            << std::endl;
+  std::cout << "Semangat " << (3 + 6) * 5 << std::endl;
+}
