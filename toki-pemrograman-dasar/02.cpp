e8e4599578e494fe764ffd67367477b5b3310b24
Ambadaya Gaman <ambadayaakun@gmail.com>
2024-08-19 13:39:13 +0800

    finished 02

---
+++ toki-pemrograman-dasar/02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int tanggal = 15;
+  int tahun = 2023;
+  string bulan = "Januari";
+
+  cout << tahun + 10 << "-" << bulan << "-" << tanggal + 7 << std::endl;
+}
